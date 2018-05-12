70ad16f45cbfe84d05823373578fd8b76e7fe3bb
zhijunzhou <zhijun.zhou@hpe.com>
2018-05-12 11:26:16 +0800

    申请助学金

---
+++ src/views/form/Grant.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <mt-field label="姓名" placeholder="" v-model="name"></mt-field>
+    <mt-field label="地区" placeholder="" v-model="regArea"></mt-field>
+    <mt-field label="手机号" placeholder="" type="tel" v-model="telphone"></mt-field>
+    <mt-field label="助学金金额" placeholder="" v-model="scholarship"></mt-field>
+    <mt-radio
+      title="申请原因"
+      align="right"
+      v-model="applyReason"
+      :options="['家庭贫困', '经济紧张', '工作时间短', '还款压力大', '在校生', '军人', '宝妈', '其他']">
+    </mt-radio>
+    <div class="submit">
+      <mt-button type="primary">生成基本信息</mt-button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'grant',
+  data() {
+    return {
+      name: '',
+      telphone: '',
+      regArea: '',
+      scholarship: '',
+      applyReason: ''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.mint-popup {
+  width: 100%;
+}
+
+.submit {
+  width: 100%;
+  padding: 10px;
+}
+
+.mint-button--primary {
+  width: 100%;
+  background-color: #1c51b3;
+}
+</style>
