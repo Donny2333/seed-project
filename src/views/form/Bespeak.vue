6dee51927711c74dcea7017c57db5d99f0166972
zhijunzhou <zhijun.zhou@hpe.com>
2018-05-12 14:17:02 +0800

    修改单选样式

--- src/views/form/Bespeak.vue
+++ src/views/form/Bespeak.vue
@@ -3,12 +3,12 @@
     <mt-field label="姓名" placeholder="" v-model="name"></mt-field>
     <mt-field label="电话" placeholder="" type="tel" v-model="telphone"></mt-field>
     <mt-field label="专业" placeholder="" v-model="subject"></mt-field>
-    <mt-radio
-      title="班型"
-      align="right"
-      v-model="classType"
-      :options="['4980', '7980', '8800', '12800', '15960']">
-    </mt-radio>
+    <mt-cell title="班型" :value="classType.value" is-link>
+      <div style="z-index: 2000" @click="popup(classType)">{{classType.value}}</div>
+      <mt-popup v-model="classType.showPopup" position="bottom">
+        <mt-picker :slots="classType.slots" @change="onClassTypeChange"></mt-picker>
+      </mt-popup>
+    </mt-cell>
     <mt-field label="预约时间" placeholder="" v-model="orderTime"></mt-field>
     <mt-field label="分校地址" placeholder="" v-model="regArea"></mt-field>
     <div class="submit">
@@ -26,9 +26,29 @@ export default {
       name: '',
       telphone: '',
       subject: '',
-      classType: '',
+      // classType: '',
       orderTime: '',
-      regArea: ''
+      regArea: '',
+      classType: {
+        value: '4980',
+        showPopup: false,
+        slots: [
+          {
+            flex: 1,
+            values: ['4980', '7980', '8800', '12800', '15960'],
+            className: 'slot3',
+            textAlign: 'center'
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    popup: function(template) {
+      template.showPopup = true
+    },
+    onClassTypeChange(picker, values) {
+      picker.getSlotValue(0) && (this.classType.value = picker.getSlotValue(0))
     }
   }
 }
--- src/views/form/Discount.vue
+++ src/views/form/Discount.vue
@@ -3,18 +3,18 @@
     <mt-field label="姓名" placeholder="" v-model="name"></mt-field>
     <mt-field label="地区" placeholder="" v-model="regArea"></mt-field>
     <mt-field label="手机号" placeholder="" type="tel" v-model="telphone"></mt-field>
-    <mt-radio
-      title="优惠券额度"
-      align="right"
-      v-model="discountQuota"
-      :options="['200', '800', '1000', '九折', '八五折', '八折', '七折']">
-    </mt-radio>
-    <mt-radio
-      title="申请原因"
-      align="right"
-      v-model="applyReason"
-      :options="['家庭贫困', '经济紧张', '工作时间短', '还款压力大', '在校生', '军人', '宝妈', '其他']">
-    </mt-radio>
+    <mt-cell title="优惠券额度" :value="discountQuota.value" is-link>
+      <div style="z-index: 2000" @click="popup(discountQuota)">{{discountQuota.value}}</div>
+      <mt-popup v-model="discountQuota.showPopup" position="bottom">
+        <mt-picker :slots="discountQuota.slots" @change="onDiscountQuotaChange"></mt-picker>
+      </mt-popup>
+    </mt-cell>
+    <mt-cell title="申请原因" :value="applyReason.value" is-link>
+      <div style="z-index: 2000" @click="popup(applyReason)">{{applyReason.value}}</div>
+      <mt-popup v-model="applyReason.showPopup" position="bottom">
+        <mt-picker :slots="applyReason.slots" @change="onApplyReasonChange"></mt-picker>
+      </mt-popup>
+    </mt-cell>
     <div class="submit">
       <mt-button type="primary">生成基本信息</mt-button>
     </div>
@@ -30,8 +30,41 @@ export default {
       name: '',
       telphone: '',
       regArea: '',
-      discountQuota: '',
-      applyReason: ''
+      discountQuota: {
+        value: '200',
+        showPopup: false,
+        slots: [
+          {
+            flex: 1,
+            values: ['200', '800', '1000', '九折', '八五折', '八折', '七折'],
+            className: 'slot3',
+            textAlign: 'center'
+          }
+        ]
+      },
+      applyReason: {
+        value: '家庭贫困',
+        showPopup: false,
+        slots: [
+          {
+            flex: 1,
+            values: ['家庭贫困', '经济紧张', '工作时间短', '还款压力大', '在校生', '军人', '宝妈', '其他'],
+            className: 'slot3',
+            textAlign: 'center'
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+    popup: function(template) {
+      template.showPopup = true
+    },
+    onApplyReasonChange(picker, values) {
+      picker.getSlotValue(0) && (this.applyReason.value = picker.getSlotValue(0))
+    },
+    onDiscountQuotaChange(picker, values) {
+      picker.getSlotValue(0) && (this.discountQuota.value = picker.getSlotValue(0))
     }
   }
 }
